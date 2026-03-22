455ae51101fbeca2775f8034733d04dca235b51a
yuu1111 <yuu210yuu@gmail.com>
2026-03-22 19:51:34 +0900

    refactor: ビルドスクリプトを設定ファイルに分離

---
+++ scripts/build.ts
@@ -0,0 +1,22 @@
+import { Glob } from "bun";
+
+const entrypoints = [
+	"src/main.ts",
+	...new Glob("src/{workers,tools}/*.ts").scanSync("."),
+];
+
+const result = await Bun.build({
+	entrypoints,
+	outdir: "dist",
+	root: "src",
+	target: "node",
+	format: "esm",
+	splitting: true,
+});
+
+if (!result.success) {
+	for (const log of result.logs) {
+		console.error(log);
+	}
+	process.exit(1);
+}
